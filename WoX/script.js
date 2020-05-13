769b559293074756d20eacc41ab647638478c1b0
12944qwerty <qwerty12944qwerty@gmail.com>
2020-05-13 02:31:46 -0500

    cleaned

--- WoX/script.js
+++ WoX/script.js
@@ -1,15 +1,9 @@
-
-	console.log("DOM fire");
-    	document.getElementById("welcoming-btn").addEventListener("click", () => {console.log("click fire");
-        	var title = document.getElementById("welcoming-title").value;
-			var message = document.getElementById("welcoming-message").value;
-			var link = document.getElementById("welcoming-link");
-			
-			var output = "javascript:(function(a){a.value=a.value.slice(0,a.selectionStart)+\"" + message.trim() +  "\"+a.value.slice(a.selectionEnd);})(document.activeElement);";
-			
-			console.log(output)
-			
-			link.innerText = title;
-			link.href = output;
-    	});
-		
+document.getElementById("welcoming-btn").addEventListener("click", () => {
+       	var title = document.getElementById("welcoming-title").value;
+	var message = document.getElementById("welcoming-message").value;
+	var link = document.getElementById("welcoming-link");
+	var output = "javascript:(function(a){a.value=a.value.slice(0,a.selectionStart)+\"" + message.trim() +  "\"+a.value.slice(a.selectionEnd);})(document.activeElement);";
+	
+	link.innerText = title;
+	link.href = output;
+});
