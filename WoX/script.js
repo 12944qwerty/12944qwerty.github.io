ff5fe375379c86955323745024ae643cf7f3d891
12944qwerty <qwerty12944qwerty@gmail.com>
2020-05-20 05:59:16 -0500

    Update script.js

--- WoX/script.js
+++ WoX/script.js
@@ -2,7 +2,7 @@ document.getElementById("welcoming-btn").addEventListener("click", () => {
        	var title = document.getElementById("welcoming-title").value;
 	var message = document.getElementById("welcoming-message").value;
 	var link = document.getElementById("welcoming-link");
-	var output = "javascript:(function(a){a.value=a.value.slice(0,a.selectionStart)+\"" + message.trim() +  "\"+a.value.slice(a.selectionEnd);})(document.activeElement);";
+	var output = "javascript:(function(a){a.value=a.value.slice(0,a.selectionStart)+\"" + message.trim() +  "\"+a.value.slice(a.selectionEnd);document.querySelectorAll('[value="Send"]')[0].click();})(document.getElementById("post"));";
 	
 	link.innerText = title;
 	link.href = output;
